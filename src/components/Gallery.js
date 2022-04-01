1113cc7343b7fdf256d96f49ed7c83e9950f4b7d
Filipe Valente Gomes <filipego@filipes-mbp.myfiosgateway.com>
2022-03-31 22:56:35 -0400

    “added_gallery”

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,78 @@
+import gal_img_1 from "../img/gal-1.jpeg";
+import gal_img_2 from "../img/gal-2.jpeg";
+import gal_img_3 from "../img/gal-3.jpeg";
+import gal_img_4 from "../img/gal-4.jpeg";
+import gal_img_5 from "../img/gal-5.jpeg";
+import gal_img_6 from "../img/gal-6.jpeg";
+import gal_img_7 from "../img/gal-7.jpeg";
+import gal_img_8 from "../img/gal-8.jpeg";
+import gal_img_9 from "../img/gal-9.jpeg";
+import gal_img_10 from "../img/gal-10.jpeg";
+import gal_img_11 from "../img/gal-11.jpeg";
+import gal_img_12 from "../img/gal-12.jpeg";
+import gal_img_13 from "../img/gal-13.jpeg";
+import gal_img_14 from "../img/gal-14.jpeg";
+
+
 function Gallery() {
   return (
     <section className="gallery">
-      Gallery
+      <figure className="gallery__item gallery__item--1">
+        <img src={gal_img_1} alt="Gallery image 1" class="gallery__img"/>
+      </figure>
+
+      <figure className="gallery__item gallery__item--2">
+        <img src={gal_img_2} alt="Gallery image 2" class="gallery__img"/>
+      </figure>
+
+      <figure className="gallery__item gallery__item--3">
+        <img src={gal_img_3} alt="Gallery image 3" class="gallery__img"/>
+      </figure>
+
+      <figure className="gallery__item gallery__item--4">
+        <img src={gal_img_4} alt="Gallery image 4" class="gallery__img"/>
+      </figure>
+
+      <figure className="gallery__item gallery__item--5">
+        <img src={gal_img_5} alt="Gallery image 5" class="gallery__img"/>
+      </figure>
+
+      <figure className="gallery__item gallery__item--6">
+        <img src={gal_img_6} alt="Gallery image 6" class="gallery__img"/>
+      </figure>
+
+      <figure className="gallery__item gallery__item--7">
+        <img src={gal_img_7} alt="Gallery image 7" class="gallery__img"/>
+      </figure>
+
+      <figure className="gallery__item gallery__item--8">
+        <img src={gal_img_8} alt="Gallery image 8" class="gallery__img"/>
+      </figure>
+
+      <figure className="gallery__item gallery__item--9">
+        <img src={gal_img_9} alt="Gallery image 9" class="gallery__img"/>
+      </figure>
+
+      <figure className="gallery__item gallery__item--10">
+        <img src={gal_img_10} alt="Gallery image 10" class="gallery__img"/>
+      </figure>
+
+      <figure className="gallery__item gallery__item--11">
+        <img src={gal_img_11} alt="Gallery image 11" class="gallery__img"/>
+      </figure>
+
+      <figure className="gallery__item gallery__item--12">
+        <img src={gal_img_12} alt="Gallery image 12" class="gallery__img"/>
+      </figure>
+
+      <figure className="gallery__item gallery__item--13">
+        <img src={gal_img_13} alt="Gallery image 13" class="gallery__img"/>
+      </figure>
+
+      <figure className="gallery__item gallery__item--14">
+        <img src={gal_img_14} alt="Gallery image 14" class="gallery__img"/>
+      </figure>
+      
     </section>
   );
 }
