e10598f582bc3c36a506e952e68037dc0a97a0f9
Filipe Valente Gomes <filipego@filipes-mbp.myfiosgateway.com>
2022-04-02 00:31:08 -0400

    “fixed_reuseable_component_Realtors”

--- src/components/RealtorsList.js
+++ src/components/RealtorsList.js
@@ -1,22 +1,17 @@
 import React from "react";
 
+  function RealtorsList(props) {
+    const {realtor_img, realtors__img_alt, name, sold } = props;
 
-class RealtorsList extends React.Component{
-    
-    render(){
-
-      const {realtor_img, realtors__img_alt, name, sold } = this.props;
-
-      return (
-        <React.Fragment>
-            <img src={realtor_img} alt={realtors__img_alt} className="realtors__img"/>
-            <div className="realtors__details">
-                <h4 className="heading-4 heading-4--light">{name}</h4>
-                <p className="realtors__sold">{sold}</p>
-            </div>
-        </React.Fragment>
-      );
-    }
+    return (
+      <React.Fragment>
+          <img src={realtor_img} alt={realtors__img_alt} className="realtors__img"/>
+          <div className="realtors__details">
+              <h4 className="heading-4 heading-4--light">{name}</h4>
+              <p className="realtors__sold">{sold}</p>
+          </div>
+      </React.Fragment>
+    );
   }
 
 export default RealtorsList;
