712aae5b41d71ce60838e1fe0c51ed8683c9a1ac
Filipe Valente Gomes <filipego@filipes-mbp.myfiosgateway.com>
2022-04-04 02:30:03 -0400

    “Fixed_some_errors”

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends React.Component{
           <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>}/>
+            <Route path="*" element={<Home/>}/>
           </Routes>
           <Footer/>
         </Router>
