ea513010a136bfa8750bcba3d3568c0d7104a5bf
Filipe Valente Gomes <filipego@filipes-mbp.myfiosgateway.com>
2022-04-04 03:00:38 -0400

    “Fixed_some_errors”

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router,Routes, Route, Link } from "react-router-dom";
+import {HashRouter as Router,Routes, Route, Link } from "react-router-dom";
 import './Base.scss';
 import Sidebar from "./components/Sidebar";
 import Home from "./components/Home";
